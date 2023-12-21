1d64a660326b75ee1c7ec54716aa1ea0f346bb1e
Ben Attenborough <ben.attenborough@featurespace.co.uk>
2023-12-21 18:45:35 +0000

    Day 10 work

--- src/day10/main.test.ts
+++ src/day10/main.test.ts
@@ -1,5 +1,10 @@
 import * as IO from "../helpers/io.ts";
-import { Day10 } from "./main.ts";
+import {
+  Day10,
+  addPoints,
+  getOrthogonalCells,
+  getOrthogonalCellsSafely,
+} from "./main.ts";
 
 let inputTest: IO.result = {
   isSuccess: false,
@@ -29,6 +34,43 @@ beforeAll(async () => {
   }
 });
 
+test("10-addPoints", () => {
+  expect(addPoints({ x: 5, y: 5 }, { x: -1, y: +1 })).toStrictEqual({
+    x: 4,
+    y: 6,
+  });
+  expect(addPoints({ x: -1, y: -1 }, { x: -1, y: +1 })).toStrictEqual({
+    x: -2,
+    y: 0,
+  });
+});
+
+test("10-1-getOrthogonalCells", () => {
+  console.log(getOrthogonalCells({ x: 0, y: 0 }));
+  expect(getOrthogonalCells({ x: 0, y: 0 })).toStrictEqual([
+    { x: 0, y: -1 },
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+    { x: -1, y: 0 },
+  ]);
+});
+
+test("10-getOrthongonalCellsSafely", () => {
+  console.log("foo", getOrthogonalCellsSafely({ x: 1, y: 0 }, 2, 2));
+  expect(getOrthogonalCellsSafely({ x: 0, y: 0 }, 2, 2)).toStrictEqual([
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+  ]);
+  expect(getOrthogonalCellsSafely({ x: 1, y: 0 }, 2, 2)).toStrictEqual([
+    { x: 1, y: 1 },
+    { x: 0, y: 0 },
+  ]);
+  expect(getOrthogonalCellsSafely({ x: 1, y: 1 }, 2, 2)).toStrictEqual([
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+  ]);
+});
+
 test("10-1-test", () => {
   if (inputTest.isSuccess) {
     expect(Day10.partOne(inputTest.value)).toBe(0);
@@ -37,26 +79,26 @@ test("10-1-test", () => {
   }
 });
 
-test("10-1-real", () => {
-  if (inputReal.isSuccess) {
-    expect(Day10.partOne(inputReal.value)).toBe(0);
-  } else {
-    console.error(inputReal.error);
-  }
-});
+// test("10-1-real", () => {
+//   if (inputReal.isSuccess) {
+//     expect(Day10.partOne(inputReal.value)).toBe(0);
+//   } else {
+//     console.error(inputReal.error);
+//   }
+// });
 
-test("10-2-test", () => {
-  if (inputTest.isSuccess) {
-    expect(Day10.partTwo(inputTest.value)).toBe(0);
-  } else {
-    console.error(inputTest.error);
-  }
-});
+// test("10-2-test", () => {
+//   if (inputTest.isSuccess) {
+//     expect(Day10.partTwo(inputTest.value)).toBe(0);
+//   } else {
+//     console.error(inputTest.error);
+//   }
+// });
 
-test("10-2-real", () => {
-  if (inputReal.isSuccess) {
-    expect(Day10.partTwo(inputReal.value)).toBe(0);
-  } else {
-    console.error(inputReal.error);
-  }
-});
+// test("10-2-real", () => {
+//   if (inputReal.isSuccess) {
+//     expect(Day10.partTwo(inputReal.value)).toBe(0);
+//   } else {
+//     console.error(inputReal.error);
+//   }
+// });
--- src/day10/main.ts
+++ src/day10/main.ts
@@ -1,5 +1,9 @@
 export const Day10 = {
   partOne: (input: string): number => {
+    const result = input.split("\n").map((x) => x.split(""));
+    console.log(result);
+    const startLocation = findInGrid(`S`, result);
+    console.log(startLocation);
     return 0;
   },
 
@@ -7,3 +11,117 @@ export const Day10 = {
     return 0;
   },
 };
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+export function findInGrid(char: string, grid: String[][]): Point {
+  let x = 0;
+  let y = 0;
+  grid.forEach((yVal, yIdex) => {
+    yVal.forEach((xVal, xIdx) => {
+      if (xVal === char) {
+        x = xIdx;
+        y = yIdex;
+      }
+    });
+  });
+  return { x, y };
+}
+
+const Pipes = new Map<string, Point[]>([
+  [
+    "|",
+    [
+      { x: 0, y: -1 },
+      { x: 0, y: 1 },
+    ],
+  ],
+  [
+    "-",
+    [
+      { x: -1, y: 0 },
+      { x: 1, y: 0 },
+    ],
+  ],
+  [
+    "L",
+    [
+      { x: 0, y: -1 },
+      { x: 1, y: 0 },
+    ],
+  ],
+  [
+    "J",
+    [
+      { x: -1, y: 0 },
+      { x: -1, y: 0 },
+    ],
+  ],
+  [
+    "7",
+    [
+      { x: -1, y: 0 },
+      { x: 0, y: 1 },
+    ],
+  ],
+  [
+    "F",
+    [
+      { x: 1, y: 0 },
+      { x: 0, y: 1 },
+    ],
+  ],
+  [
+    ".",
+    [
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+    ],
+  ],
+]);
+
+export function oppositeSide(number: number): number {
+  if (number === 0) {
+    return 0;
+  } else {
+    if (number < 0) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+}
+
+export function addPoints(pointA: Point, pointB: Point): Point {
+  return {
+    x: pointA.x + pointB.x,
+    y: pointA.y + pointB.y,
+  };
+}
+
+export function getOrthogonalCells(origin: Point): Point[] {
+  return [
+    addPoints(origin, { x: 0, y: -1 }),
+    addPoints(origin, { x: 1, y: 0 }),
+    addPoints(origin, { x: 0, y: 1 }),
+    addPoints(origin, { x: -1, y: 0 }),
+  ];
+}
+
+export function getOrthogonalCellsSafely(
+  origin: Point,
+  maxWidth: number,
+  maxDepth: number,
+): Point[] {
+  return getOrthogonalCells(origin).filter(
+    (point) =>
+      point.x >= 0 && point.y >= 0 && point.x < maxWidth && point.y < maxDepth,
+  );
+}
+
+// export function pipeConnects(pipeA: Point[], pipeB: Point[]) {
+//   if (pipeA[0])
+// }
